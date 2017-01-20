01118e288c3ec57e7a17e6d4d2ff61cae9adcd65
Per Andersson <per@westcoastcode.se>
2017-01-20 22:14:14 +0100

    Added a player that can be moved

---
+++ Assets/GameObjects/Player/MovePlayerComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class MovePlayerComponent : MonoBehaviour 
+{
+	[SerializeField]
+	private float speed = 3.0f;
+
+	private PlayerMotor playerMotor;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerMotor = GetComponent<PlayerMotor> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void FixedUpdate() 
+	{
+		var vertical = Input.GetAxisRaw ("Vertical");
+		var horizontal = Input.GetAxisRaw ("Horizontal");
+
+		playerMotor.MoveForward (vertical * speed);
+		playerMotor.MoveRight (horizontal * speed);
+	}
+}
---
+++ Assets/GameObjects/Player/PlayerMotor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour 
+{
+	private Rigidbody rb;
+
+	private float forward = 0;
+	private float right = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		rb.velocity = new Vector3 (right, 0, forward);
+	}
+
+	public void MoveForward(float forward)
+	{
+		this.forward = forward;
+	}
+
+	public void MoveRight(float right)
+	{
+		this.right = right;
+	}
+}
