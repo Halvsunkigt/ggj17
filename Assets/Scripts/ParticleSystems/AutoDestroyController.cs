bc03aae57f6afad4887d61a9c871fb00e882d63a
Per Andersson <per@per-mbp.local>
2017-01-21 21:27:23 +0100

    Added basic particle system and auto delete controller for it

---
+++ Assets/Scripts/ParticleSystems/AutoDestroyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class AutoDestroyController : MonoBehaviour 
+{
+	private ParticleSystem system;
+
+	void Start ()
+	{
+		system = GetComponent<ParticleSystem> ();	
+	}
+
+	void FixedUpdate ()
+	{
+		if (!system.IsAlive ()) {
+			Destroy (gameObject);
+		}	
+	}
+}
