501d92c67b8999c85dec6beca3b3e427a7356928
Per Andersson <per@per-mbp.local>
2017-01-21 13:36:34 +0100

    Changed Fire1 to Fire1_Player0 and added some tags. Also put the player index of a player to it's own controller

--- Assets/Scripts/Player/MovePlayerComponent.cs
+++ Assets/Scripts/Player/MovePlayerComponent.cs
@@ -2,24 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MovementController))]
+[RequireComponent(typeof(MovementController), typeof(PlayerController))]
 public class MovePlayerComponent : MonoBehaviour 
 {
-	[SerializeField]
-	private int playerIndex = 0;
-
-	private Vector3 rotate;
+	private PlayerController player;
 	private MovementController movement;
+	private Vector3 rotate;
 
 	void Start () 
 	{
+		player = GetComponent<PlayerController> ();
 		movement = GetComponent<MovementController> ();
 	}
 
 	void FixedUpdate() 
 	{
-		var v = Input.GetAxisRaw ("Vertical_Player" + playerIndex);
-		var h = Input.GetAxisRaw ("Horizontal_Player" + playerIndex);
+		var v = Input.GetAxisRaw ("Vertical_Player" + player.playerIndex);
+		var h = Input.GetAxisRaw ("Horizontal_Player" + player.playerIndex);
 
 		// we use world-relative directions in the case of no main camera
 		rotate = v * Vector3.forward + h * Vector3.right;
--- Assets/Scripts/Player/ObjectRangeController.cs
+++ Assets/Scripts/Player/ObjectRangeController.cs
@@ -2,15 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectRangeController : MonoBehaviour {
+[RequireComponent(typeof(SphereCollider), typeof(PlayerController))]
+public class ObjectRangeController : MonoBehaviour 
+{
+	[SerializeField]
+	private string[] carryableObjects = new string[] {
+		"Box", "Player", "Turret"
+	};
 
-	// Use this for initialization
-	void Start () {
+	private PlayerController player;
+	private GameObject carryingObject;
+	private GameObject collidingObject;
+
+	void Start () 
+	{
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void FixedUpdate() 
+	{
+		if (!IsCollidingWithObject ()) {
+			return;
+		}
+
+		if (Input.GetButtonDown("Fire1_Player" + player.playerIndex)) {
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (IsCarryingObject()) {
+			return;
+		}
+
+		for (var i = 0; i < carryableObjects.Length; ++i) {
+			if (carryableObjects [i].Equals (other.tag)) {
+				collidingObject = other.gameObject;
+				break;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (!IsCarryingObject (other.gameObject)) {
+			return;
+		}
+	}
+
+	private bool IsCarryingObject() 
+	{
+		return carryingObject != null;
+	}
+
+	private bool IsCollidingWithObject() 
+	{
+		return collidingObject != null;
+	}
+
+	private bool IsCarryingObject(GameObject obj)
+	{
+		return obj.Equals (carryingObject);
 	}
 }
