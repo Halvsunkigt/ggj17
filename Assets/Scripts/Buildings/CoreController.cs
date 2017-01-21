de29679b2ab84a9a99c4007ab837b334b68600b9
Per Andersson <per@per-mbp.local>
2017-01-21 21:56:37 +0100

    Added support for particle effects when the core is being damaged, destroyed and repaired

--- Assets/Scripts/Buildings/CoreController.cs
+++ Assets/Scripts/Buildings/CoreController.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class CoreController : MonoBehaviour
 {
-	public Transform hpTransform;
+	private static Quaternion UP = Quaternion.Euler (new Vector3 (-90, 0, 0));
 
-	float hp = 100f;
-	bool destroyed = false;
+	[SerializeField]
+	private Transform hpTransform;
+
+	[SerializeField]
+	private GameObject damageEffect;
+
+	[SerializeField]
+	private GameObject destroyEffect;
+
+	[SerializeField]
+	private GameObject repairEffect;
+
+	private float hp = 100f;
+	private bool destroyed = false;
 	
 	// Update is called once per frame
 	void Update ()
@@ -15,7 +27,7 @@ public class CoreController : MonoBehaviour
 		UpdateHp ();
 	}
 
-	void UpdateHp ()
+	private void UpdateHp ()
 	{
 		if (hp < 100f) {
 			if (destroyed) {
@@ -24,7 +36,7 @@ public class CoreController : MonoBehaviour
 					HasBeenRebuilt ();
 				}
 			} else {
-				hp += Time.deltaTime * 1f;
+				GiveHealth (Time.deltaTime * 1f);
 			}
 		}
 	}
@@ -32,22 +44,43 @@ public class CoreController : MonoBehaviour
 	public void TakeDamage (float damage)
 	{
 		hp -= damage;
-
-		hpTransform.localScale = new Vector3 (hp / 100f, hpTransform.localScale.y, hpTransform.localScale.z);
-		hpTransform.localPosition = new Vector3 (-0.5f + (hp / 100f) / 2, hpTransform.localPosition.y, hpTransform.localPosition.z);
+		UpdateHealthBar (hp);
 
 		if (hp < 0f) {
 			HasBeenDestroyed ();
+		} else {
+			if (damageEffect != null) {
+				GameObject.Instantiate (damageEffect, gameObject.transform.position, UP);
+			}
 		}
 	}
 
-	void HasBeenDestroyed ()
+	public void GiveHealth(float health) 
+	{
+		hp = Mathf.Max (hp + health, 100.0f);
+		UpdateHealthBar (hp);
+		if (repairEffect != null) {
+			GameObject.Instantiate (repairEffect, gameObject.transform.position, UP);
+		}
+	}
+
+	private void UpdateHealthBar(float hp)
+	{
+		hpTransform.localScale = new Vector3 (hp / 100f, hpTransform.localScale.y, hpTransform.localScale.z);
+		hpTransform.localPosition = new Vector3 (-0.5f + (hp / 100f) / 2, hpTransform.localPosition.y, hpTransform.localPosition.z);
+	}
+
+	private void HasBeenDestroyed ()
 	{
 		destroyed = true;
 		hp = 0f;
+
+		if (destroyEffect != null) {
+			GameObject.Instantiate (destroyEffect, gameObject.transform.position, UP);
+		}
 	}
 
-	void HasBeenRebuilt ()
+	private void HasBeenRebuilt ()
 	{
 		destroyed = false;
 		hp = 100f;
