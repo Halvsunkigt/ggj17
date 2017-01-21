9e52a4c50a6a68f926f72f17e5b7c6b364b631ec
Mikael Mattsson <mikael.p.mattsson@gmail.com>
2017-01-21 14:02:44 +0100

    CoreController Update.

--- Assets/Scripts/Buildings/CoreController.cs
+++ Assets/Scripts/Buildings/CoreController.cs
@@ -2,15 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoreController : MonoBehaviour {
+public class CoreController : MonoBehaviour
+{
+	float hp = 100f;
+	bool destroyed = false;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		UpdateHp ();
+	}
+
+	void UpdateHp ()
+	{
+		if (hp < 100f) {
+			if (destroyed) {
+				hp += Time.deltaTime * 2f;
+				if (rebuild >= 100f) {
+					HasBeenRebuilt ();
+				}
+			} else {
+				hp += Time.deltaTime * 1f;
+			}
+		}
+	}
+
+	public void TakeDamage (float damage)
+	{
+		hp -= damage;
+
+		if (hp < 0f) {
+			HasBeenDestroyed ();
+		}
+	}
+
+	void HasBeenDestroyed ()
+	{
+		destroyed = true;
+		hp = 0f;
+	}
+
+	void HasBeenRebuilt ()
+	{
+		destroyed = false;
+		hp = 100f;
 	}
 }
