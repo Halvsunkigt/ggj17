49fd7790b19913b654d8cc78ac5b71970627716f
Mikael Mattsson <mikael.p.mattsson@gmail.com>
2017-01-21 16:17:26 +0100

    Shoot closest enemy

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class BulletController : MonoBehaviour
+{
+	public float speed;
 }
--- Assets/Scripts/Buildings/TurretController.cs
+++ Assets/Scripts/Buildings/TurretController.cs
@@ -21,20 +21,63 @@ public class TurretController : MonoBehaviour
 	void Update ()
 	{
 		TryToShoot ();
-
 	}
 
 	void TryToShoot ()
 	{
-		if (nextShoot < Time.timeSinceLevelLoad) {
-			
+		if (nextShoot > Time.timeSinceLevelLoad) {
+			return;
 		}
 
 		// Todo: Check ammo
+
+		EnemyController enemy = GetClosestEnemy ();
+
+		if (enemy == null) {
+			return;
+		}
+
+		GameObject bullet = Instantiate (bulletPrefab);
+		bullet.transform.position = transform.position;
+		bullet.transform.LookAt (enemy.transform);
+
+
+		Vector3 direction = enemy.transform.position - transform.position;
+		bullet.GetComponent<Rigidbody> ().AddForce (direction.normalized * bullet.GetComponent<BulletController> ().speed);
+
+		nextShoot = Time.timeSinceLevelLoad + shootInterval;
 	}
 
 	EnemyController GetClosestEnemy ()
 	{
-		return GameObject.FindObjectOfType<EnemyController> ();
+		Collider[] colliders = Physics.OverlapSphere (transform.position, range);
+		EnemyController closest = null;
+		EnemyController enemy = null;
+		float distance = range;
+
+		//Debug.Log (colliders.Length);
+
+		foreach (Collider hit in colliders) {
+
+			enemy = hit.GetComponent<EnemyController> ();
+
+			if (enemy == null) {
+				continue;
+			}
+
+			if (closest == null || Vector3.Distance (transform.position, hit.transform.position) <= distance) {
+				closest = enemy;
+			}
+		}
+
+		return closest;
 	}
+
+	#if UNITY_EDITOR
+	void OnDrawGizmosSelected ()
+	{
+		UnityEditor.Handles.color = Color.white;
+		UnityEditor.Handles.DrawWireDisc (transform.position, transform.up, range);
+	}
+	#endif
 }
