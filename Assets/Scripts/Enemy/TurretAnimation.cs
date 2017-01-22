a32b00fa1a8868b36fea9eed21f9d43ab2a69308
Ludvig Christensson <Ludvig@Ludvigs-MacBook-Pro.local>
2017-01-22 14:44:33 +0100

    Replaced player walk animation and implemented turret animation callback

--- Assets/Scripts/Enemy/TurretAnimation.cs
+++ Assets/Scripts/Enemy/TurretAnimation.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurretAnimation : MonoBehaviour {
+public class TurretAnimation : StateMachineBehaviour {
 
 	[SerializeField]
 	private Animator animator;
+
+	private Callback latestCallback;
 
 	public delegate void Callback();
 	public void SetShooting (bool shooting, Callback callback)
 	{
 		animator.SetBool ("Shooting", shooting);
+		latestCallback = callback;
+	}
+
+	override public void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		if (stateInfo.IsName ("Idle"))
+		{
+			Debug.Log ("Woop");
+			if (latestCallback != null)
+			{
+				latestCallback ();
+			}
+		}
 	}
 }
