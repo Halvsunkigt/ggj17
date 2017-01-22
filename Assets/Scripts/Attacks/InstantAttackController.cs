15c4c22cede576b8dd7588a553ffbceedee37076
Per Andersson <per@per-mbp.local>
2017-01-22 10:22:00 +0100

    Instant attacks available

---
+++ Assets/Scripts/Attacks/InstantAttackController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantAttackController : ProjectileAttack 
+{
+	[SerializeField]
+	private float damage = 20;
+
+	public override void AttackTarget(Vector3 from, GameObject target) {
+		EnemyController enemy = target.GetComponent<EnemyController> ();
+		if (enemy != null) {
+			enemy.TakeDamage (damage);
+		}
+	}
+}
