b8674727be6e0c4fb2e931c150b77014528b88ee
Per Andersson <per@westcoastcode.se>
2017-01-22 13:08:39 +0100

    Added UI for displaying coins

---
+++ Assets/Menu/InGameCanvas/CoinsAmountController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsAmountController : MonoBehaviour {
+
+	private GameState gameState;
+
+	// Use this for initialization
+	void Start ()
+	{
+		gameState = GameObject.Find ("GameState").GetComponent<GameState> ();
+	}
+
+	void Update ()
+	{
+		var text = GetComponent<Text> ();
+		text.text = gameState.NumCoins.ToString ();
+	}
+}
