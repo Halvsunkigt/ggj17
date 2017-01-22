9c79959cca27674824a3ebc25bd7da91352c3a88
Ludvig Christensson <Ludvig@Ludvigs-MacBook-Pro.local>
2017-01-22 13:49:24 +0100

    Minor changes

---
+++ Assets/TurretAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAnimation : MonoBehaviour {
+
+	[SerializeField]
+	private Animator animator;
+
+	public void SetShooting (bool shooting)
+	{
+		animator.SetBool ("Shooting", shooting);
+	}
+}
